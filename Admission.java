59084b952032d3130fb46d8c4995b091cbecce60
Scot Willard programming hub <Scottwillard9698@gmail.com>
2020-09-17 09:40:17 -0400

    Update Admission.java

--- Admission.java
+++ Admission.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 public class Admission {
     public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         double GPA;
         int testScore;
         int counter = 1;
         while (counter != 0) {
             System.out.println("Enter the GPA: ");
-            GPA = a.nextDouble();
+            GPA = in.nextDouble();
             while (GPA <= 0.0 || GPA > 4.0) {
                 System.out.println("Error. Try again.");
-                GPA = a.nextDouble();
+                GPA = in.nextDouble();
             }
             System.out.println("Enter the admission test score: ");
-            testScore = a.nextInt();
+            testScore = in.nextInt();
             while (testScore < 0.0 || testScore > 100.0) {
                 System.out.println("Error. Try again.");
-                testScore = a.nextInt();
+                testScore = in.nextInt();
             }
             if ((GPA >= 3.0 && testScore >= 60) || (GPA <= 3.0 && testScore >= 80))
                 System.out.println("Accept");
             else
                 System.out.println("Reject");
             System.out.println("To evaluate another student, press 1. To quit Press 0");
-            counter = a.nextInt();
+            counter = in.nextInt();
         }//while
     }//main
 }//Class
